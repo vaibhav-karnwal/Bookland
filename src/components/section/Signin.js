a7a9e31017dcad8a757deebf0d1d4f4347eb952d
vaibhav-karnwal <vaibhavkarnwal2812@gmail.com>
2021-11-12 19:01:18 +0530

    initial commit

--- src/components/section/Signin.js
+++ src/components/section/Signin.js
@@ -1,16 +1,5 @@
 import React from 'react';
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
-
-import {
-    SignInContainer,
-    SignInTitle,
-    ButtonsBarContainer
-} from './sign-in.styles';
-
 class SignIn extends React.Component {
     constructor(props) {
     super(props);
@@ -21,33 +10,15 @@ class SignIn extends React.Component {
     };
 }
 
-handleSubmit = async event => {
-    event.preventDefault();
-
-    const { email, password } = this.state;
-
-    try {
-        await auth.signInWithEmailAndPassword(email, password);
-        this.setState({ email: '', password: '' });
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-handleChange = event => {
-    const { value, name } = event.target;
-
-    this.setState({ [name]: value });
-};
 
 render() {
     return (
-        <SignInContainer>
-            <SignInTitle>I already have an account</SignInTitle>
+        <section>
+            <h1>I already have an account</h1>
             <span>Sign in with your email and password</span>
 
             <form onSubmit={this.handleSubmit}>
-                <FormInput
+                <input
                     name='email'
                     type='email'
                     handleChange={this.handleChange}
@@ -55,7 +26,7 @@ render() {
                     label='email'
                     required
                 />
-                <FormInput
+                <input
                     name='password'
                     type='password'
                     value={this.state.password}
@@ -63,14 +34,9 @@ render() {
                     label='password'
                     required
                 />
-                <ButtonsBarContainer>
-                    <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                        Sign in with Google
-        </CustomButton>
-                </ButtonsBarContainer>
+                <button type='submit'> Sign in </button>        
             </form>
-        </SignInContainer>
+        </section>
     );
 }
 }
